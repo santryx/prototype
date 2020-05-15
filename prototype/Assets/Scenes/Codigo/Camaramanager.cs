c696b29aea02781683a4c8f7d1e9b8826317db9d
Santy Monroy <santryx.dmc@gmail.com>
2020-05-15 18:31:23 -0500

    0.01

---
+++ prototype/Assets/Scenes/Codigo/Camaramanager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camaramanager : MonoBehaviour
+{
+    public GameObject jugador;
+    public Camera camaraDelJuego;
+    
+    public GameObject[] mundoPrefab;
+    public float pJuego;
+    public float lugarseguro = 2;
+ 
+
+
+    void Start()
+    {
+        pJuego = -7;
+        
+    }
+
+   
+    void Update()
+    {
+       
+
+        if (jugador != null)
+        {
+            camaraDelJuego.transform.position = new Vector3(camaraDelJuego.transform.position.x, camaraDelJuego.transform.position.y, camaraDelJuego.transform.position.z);
+        }
+
+        while (jugador != null && pJuego < jugador.transform.position.z + lugarseguro) 
+        {
+            int indiceMundo = Random.Range(0, mundoPrefab.Length - 1);
+
+            if (pJuego < 0)
+            {
+                indiceMundo = 2;
+            }
+
+            GameObject ObjetoMundo = Instantiate(mundoPrefab[indiceMundo]);
+            ObjetoMundo.transform.SetParent(this.transform);
+            Mundo mundo = ObjetoMundo.GetComponent<Mundo>();
+            ObjetoMundo.transform.position = new Vector3(0, 0,  pJuego + mundo.tamaño / 2);
+            pJuego += mundo.tamaño;
+        }
+        
+    }
+
+}
---
+++ prototype/Assets/Scenes/Codigo/personaje.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personaje : MonoBehaviour
+{
+
+    public int fuerzaDeSaltos;
+    public int velocidadDeMov;
+    bool canJump;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        this.GetComponent<Rigidbody>().velocity = new Vector3(this.GetComponent<Rigidbody>().velocity.x, this.GetComponent<Rigidbody>().velocity.y, velocidadDeMov);
+
+        if (Input.GetKeyDown("space")  && canJump)
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0, fuerzaDeSaltos, 0));
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(-15, 0, 0);
+            StartCoroutine("parardeliz");
+        }
+
+
+        if (Input.GetKeyDown("d"))
+        {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(15, 0, 0);
+            StartCoroutine("parardeliz");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canJump = true;
+
+        if (other.tag == "Obstaculo")
+        {
+            GameObject.Destroy(this);
+            Debug.Log("perdiste");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canJump = false;
+    }
+
+
+    IEnumerator parardeliz() 
+    {
+        yield return new WaitForSeconds(0.8f);
+        this.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+    }
+
+
+}
+
+
+
